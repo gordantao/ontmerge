c8c8e5ebfdaa3d58310081f8ae8384e354037ff1
gordantao <taogordan@gmail.com>
2026-04-15 11:56:33 -0400

    Cosmetic changes: favicon and title.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import "./layout.css";
-  import favicon from "$lib/assets/favicon.svg";
+  import favicon from "$lib/assets/favicon.png";
   import { ModeWatcher } from "mode-watcher";
   let { children } = $props();
 </script>
 
 <ModeWatcher />
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><title>OntMerge</title><link rel="icon" href={favicon} /></svelte:head>
 <div class="flex flex-col h-screen">
   <main class="flex-1 overflow-hidden">
     {@render children()}
